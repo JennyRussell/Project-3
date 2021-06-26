27e27a7680a2884f05054c022fc473996b32aed2
Derrick Helgeson <dkmk1020@gmail.com>
2021-06-26 10:36:38 -0700

    first push

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { Login } from './components/login/index';
+import './components/login/style.css';
+import { Register } from './components/login/index';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+export default App;
