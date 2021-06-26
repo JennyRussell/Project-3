c3433b6861b0fa554f8d3a9a038f188be3185a2a
Jenny Russell <moosyoolka@gmail.com>
2021-06-26 15:56:04 -0700

    created styling from the welcome page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import { Login } from './components/login/index';
 import './components/login/style.css';
 import { Register } from './components/login/index';
+import Welcome from './components/Welcome';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
   return (
     <div className="App">
+      <Welcome />
       <Login />
       <Register />
     </div>
