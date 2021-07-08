9dc39497852c904915dee2d2f89bb6198f62cef1
Derrick Helgeson <dkmk1020@gmail.com>
2021-07-07 19:28:21 -0700

    update

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,23 +1,22 @@
-import React, { useState } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+// import React, { useState } from "react";
+// import { Calendar, momentLocalizer } from "react-big-calendar";
+// import moment from "moment";
+// import "react-big-calendar/lib/css/react-big-calendar.css";
 
+// function MyCalendar(props) {
+//     // set event state --> if we want to update events, we'll use setEvents({...events, newState});
 
-function MyCalendar(props) {
-    // set event state --> if we want to update events, we'll use setEvents({...events, newState});
-
-    console.log('props: ', props);
-    return (
-    <Calendar
-      localizer={props.localizer}
-      events={props.events}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 500 }}
-    />
-    )
-};
+//     console.log('props: ', props);
+//     return (
+//     <Calendar
+//       localizer={props.localizer}
+//       events={props.events}
+//       startAccessor="start"
+//       endAccessor="end"
+//       style={{ height: 500 }}
+//     />
+//     )
+// };
 
 // function getBirthdaysList() {
 //   const birthdaysList = [];
@@ -39,4 +38,4 @@ function MyCalendar(props) {
 //   }
 // }
 
-export default MyCalendar;
+// export default MyCalendar;
--- src/components/user-page/UserPage.js
+++ src/components/user-page/UserPage.js
@@ -1,40 +1,40 @@
-import React, {useState, useEffect} from "react";
-import People from '../people/People'
-import addUser from "../addUser.png"
-import Calendar from "../calendar/Calendar";
-import moment from "moment";
-import { momentLocalizer } from "react-big-calendar";
+import React, { useState, useEffect } from "react";
+import People from "../people/People";
+import addUser from "../addUser.png";
+// import Calendar from "../calendar/Calendar";
+// import moment from "moment";
+// import { momentLocalizer } from "react-big-calendar";
 import { useHistory } from "react-router-dom";
 // import imagePh from '../image-ph.png';
 
 function UserPage() {
   const history = useHistory();
-    useEffect(() => {
-        // call to the api to get the user's events
-        // const userEvents = API.getUserEvents
-            // .then().catch()
-            // set the event state to their events
-            // setEvents(userEvents)
-            // datetime format back from mysql -- see how that works to get moment formatted dates
-    }, [])
-    const localizer = momentLocalizer(moment);
+  useEffect(() => {
+    // call to the api to get the user's events
+    // const userEvents = API.getUserEvents
+    // .then().catch()
+    // set the event state to their events
+    // setEvents(userEvents)
+    // datetime format back from mysql -- see how that works to get moment formatted dates
+  }, []);
+  // const localizer = momentLocalizer(moment);
 
-    // would initialize as empty state useState([]);
-    const [events, setEvents] = useState([
-        {
-            start: moment("20210630,", "YYYYMMDD").toDate(),
-            end: moment().toDate(),
-            title: "Wednesday's Class",
-        },
-        {
-            start: moment("20210704", "YYYYMMDD").fromNow(),
-            end: moment().add(1, "days").toDate(),
-            title: "Independence Day",
-        },
-    ]);
+  // // would initialize as empty state useState([]);
+  // const [events, setEvents] = useState([
+  //     {
+  //         start: moment("20210630,", "YYYYMMDD").toDate(),
+  //         end: moment().toDate(),
+  //         title: "Wednesday's Class",
+  //     },
+  //     {
+  //         start: moment("20210704", "YYYYMMDD").fromNow(),
+  //         end: moment().add(1, "days").toDate(),
+  //         title: "Independence Day",
+  //     },
+  // ]);
 
-    return (
-<div>
+  return (
+    <div>
       <nav className="welcome w-100 h-24 border-2">
         <h1 className="m-4 text-6xl">Welcome</h1>
       </nav>
@@ -56,14 +56,9 @@ function UserPage() {
               alt="DVBSB"
             ></img>
           </button>
-
         </div>
       </div>
     </div>
-
-
-
-
   );
 }
 
