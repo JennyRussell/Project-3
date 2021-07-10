90451503684c2635aed3e3ecb13419ae9fbf45bc
Rwan Abdelmonem <rabdelmonem0@gmail.com>
2021-07-10 11:37:59 -0700

    added dates.js and events.js to calendar component

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import AddPerson from "./components/add-person/AddPerson";
 import AUTH from "./utils/AUTH";
 import UserContext from "./utils/UserContext";
 
+
 function App(props) {
   const [user, setUser] = useState();
 
