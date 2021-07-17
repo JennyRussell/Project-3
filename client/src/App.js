161c8ca890d6878745bfd780d51ec26f1e54eef8
Derrick Helgeson <dkmk1020@gmail.com>
2021-07-17 14:25:35 -0700

    video code added

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddPerson from "./components/add-person/AddPerson";
 import AUTH from "./utils/AUTH";
 import UserContext from "./utils/UserContext";
+import Vid from "./components/Vid";
 
 function App(props) {
   const [user, setUser] = useState();
@@ -49,6 +50,10 @@ function App(props) {
 
             <Route exact path="/add-person">
               <AddPerson />
+            </Route>
+
+            <Route exact path="/vid">
+              <Vid />
             </Route>
           </Switch>
         </Router>
